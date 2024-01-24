707c89342592b0bcc0dbd1e8d125f639b35df16e
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 01:35:33 -0600

    Archivos donde se declara la clase principal e implementa sus funciones
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Tareas/TAREA_TRES/src/matriz.cpp
@@ -0,0 +1,88 @@
+/**
+ * @file matriz.cpp
+ * @author your name (you@domain.com)
+ * @brief archivo que implementa las funciones prototipo de la Clase principal Matriz
+ * @version 0.1
+ * @date 2024-01-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "matriz.hpp"
+
+/**
+ * @brief Constructor de la clase principal Matriz
+ * 
+ * @tparam T tipo de dato de la matriz
+ * @param filas numero de filas
+ * @param columnas numero de filas
+ * @param valores vector con los valores de la matriz
+ * @throw invalid_argument lanza error si los datos de la matriz son diferentes a float, int y numeros complejos
+ */
+template <typename T>
+Matriz<T>::Matriz(size_t filas, size_t columnas, const vector<vector<T>>& valores)
+    : filas(filas), columnas(columnas), datos(valores) {
+    
+    if constexpr (!is_same<T, int>::value && !is_same<T, float>::value && !is_same<T, complex<double>>::value) {
+        throw invalid_argument("Tipo de dato no permitido. Solo se permiten int, float y complex<double>.");
+    }
+}
+
+/**
+ * @brief funcion que solicita que ingrese el numero de columnas y filas de la matris y sus elementos
+ * 
+ * @tparam T 
+ */
+template <typename T>
+void Matriz<T>::pedirDatos() {
+    cout << "Ingrese el numero de filas de la matriz: ";
+    cin >> filas;
+
+    cout << "Ingrese el numero de columnas de la matriz: ";
+    cin >> columnas;
+
+    datos.resize(filas, vector<T>(columnas));
+
+    for (size_t i = 0; i < filas; ++i) {
+        for (size_t j = 0; j < columnas; ++j) {
+            cout << "Ingrese el elemento en la posicion fila [" << i << "] y columna [" << j << "]: ";
+            cin >> datos[i][j];
+        }
+    }
+}
+
+/**
+ * @brief metodo de funcion que solicita que ingrese el tipo de operacion que desea realizar
+ * 
+ * @tparam T 
+ * @return matriz despues de la operacion
+ * @throw invalid_argument lanza un mensaje de error si la operacion ingresada no es valida
+ */
+template <typename T>
+Matriz<T> Matriz<T>::realizarOperacion() const {
+    char operacion;
+    cout << "Ingrese la operacion que desea realizar (+, - o *): ";
+    cin >> operacion;
+
+    Matriz<T> resultado(filas, columnas, datos);
+
+    for (size_t i = 0; i < filas; ++i) {
+        for (size_t j = 0; j < columnas; ++j) {
+            switch (operacion) {
+                case '+':
+                    resultado.datos[i][j] += datos[i][j];
+                    break;
+                case '-':
+                    resultado.datos[i][j] -= datos[i][j];
+                    break;
+                case '*':
+                    resultado.datos[i][j] *= datos[i][j];
+                    break;
+                default:
+                    throw invalid_argument("Operacion no valida. Ingrese una de las operaciones indicadas");
+            }
+        }
+    }
+
+    return resultado;
+}
---
+++ Tareas/TAREA_TRES/src/matriz.hpp
@@ -0,0 +1,57 @@
+/**
+ * @file matriz.hpp
+ * @author jesyrivera
+ * @brief archivo donde se declara la Clase principal Matriz
+ * @version 0.1
+ * @date 2024-01-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef MATRIZ_HPP
+#define MATRIZ_HPP
+
+#include <vector>
+#include <stdexcept>
+#include <complex>
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Se declara la clase principal Matriz representa una matriz que acepta cualquier tipo de dato
+ * @tparam T
+ */
+template <typename T>
+class Matriz {
+private:
+    vector<vector<T>> datos; ///< vector que almacena los elementos de la matriz
+    size_t filas; ///< numero de filas
+    size_t columnas; ///<numero de columnas
+
+public:
+//escribir comentarioooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+    Matriz() : filas(0), columnas(0) {}
+    /**
+     * @brief constructor de la clase Matriz
+     * @param filas 
+     * @param columnas 
+     * @param valores 
+     */
+    Matriz(size_t filas, size_t columnas, const vector<vector<T>>& valores);
+    
+    /**
+     * @brief funcion que le pide al usuario ingresar valores
+     * le pide ingresar el numero de filas y columnas de la matriz
+     * le pide ingresar los datos de la matriz
+     */
+    void pedirDatos();
+
+    /**
+     * @brief metodo de funcion que le pide al usuario el tipo de operacion que desea realizar
+     * 
+     * @throw invalid_argument lanza un mensaje de error si la operacion es erronea
+     */
+    Matriz<T> realizarOperacion() const;
+};
+
+#endif // MATRIZ_HPP
