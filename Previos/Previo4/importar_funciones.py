c03aa3de28e1e444d5d148fabe152f48898da0c4
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 23:35:58 -0600

    Archivo en Python donde se importan funciones
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo4/importar_funciones.py
@@ -0,0 +1,14 @@
+#importar funcion
+import math
+
+#se declara la variable
+square_root = math.sqrt(4)
+
+#se imprime el mensaje y la raiz
+print("Square Roof of 4 is", square_root)
+
+#exponencial
+power = pow(2, 3)
+
+#imprime el mensaje y el rsultado
+print("2 to the power 3 is", power)
