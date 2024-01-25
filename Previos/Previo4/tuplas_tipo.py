89a3cdf91ceb2a635f2820e1f05fcc21a296ecdf
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 23:47:21 -0600

    Archivo con ejemplo de los tipos de tuplas
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo4/tuplas_tipo.py
@@ -0,0 +1,20 @@
+#diferentes tipos de tuplas
+#tupla vacia
+my_tuple = ()
+#imprime la tupla
+print(my_tuple)
+
+#tupla que tiene int
+my_tuple = (1, 2, 3)
+#imprime la tupla
+print(my_tuple)
+
+#tupla mixta
+my_tuple = (1, "Hello", 3.4)
+#imprimir tupla
+print(my_tuple)
+
+#nested tupla
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+#imprimir la tupla
+print(my_tuple)
