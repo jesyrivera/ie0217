3193ee420d8d4f3268343c368fbf24ef9989bd45
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 23:48:35 -0600

    Archivo de como importar constantes y de constantes en Python
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo4/constantes.py
@@ -0,0 +1,3 @@
+#se declara una variable constante (no se puede cambiar el valor)
+PI = 3.14
+GRAVITY = 9.8
---
+++ Previos/Previo4/constantes_importar.py
@@ -0,0 +1,8 @@
+#importa la constante que se creo en otro archivo
+import constantes
+
+#imprime el valor de la primera constante
+print(constantes.PI)
+
+#imprime el valor de la segunda constante
+print(constantes.GRAVITY)
