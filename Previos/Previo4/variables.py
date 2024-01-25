8c258c4b67a7f0d7d970325188207fe02a835bf1
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 23:46:33 -0600

    Archivos con ejemplos de variales en Python
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo4/variables.py
@@ -0,0 +1,11 @@
+#se declara una variable
+site_name = 'eie.ucr.ac.cr'
+
+#se imprime el valor de la variable
+print(site_name)
+
+#le cae encima al valor que tenia antes la variable
+site_name='google.com'
+
+#imprime el nuevo valor de la variable
+print(site_name)
