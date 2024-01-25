add69c5c7065cc398c82cbbee4e0c62605928406
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 23:49:26 -0600

    Archivo de variables de como se asignan valores
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo4/asignaciones_variables.py
@@ -0,0 +1,11 @@
+# se crean variables y se inicializan
+a, b, c= 5, 3.2, 'Hello'
+
+#imprime el valor de a
+print(a)
+
+#imprime el valor de b
+print(b)
+
+#imprime el valor de c que es la palabra
+print(c)
