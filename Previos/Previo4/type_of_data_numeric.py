40110a44b16d71b09f829edaf34f35533161883d
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-24 23:45:56 -0600

    Archivos ocn ejemplos en Python de tipos de datos mapping, sequence, set, string, numeric
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo4/type_of_data_numeric.py
@@ -0,0 +1,17 @@
+#declara una variable y su valor
+num1 = 5
+
+#imprime el mensaje y el tipo de variable que concluyo que era
+print(num1, 'is of type', type(num1))
+
+#declara una variable y su valor
+num2 = 2.0
+
+#imprime el mensaje y el tipo de variable que concluyo que era
+print(num2, 'is of type', type(num2))
+
+#declara una variable compleja y su valor
+num3 = 1+2j
+
+#imprime el mensaje y el tipo de variable que concluyo que era
+print(num3, 'is of type', type(num3))
