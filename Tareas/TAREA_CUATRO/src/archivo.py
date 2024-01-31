34a9c858fb31cfb3c8bd2addbf9031e4bf627bfd
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-31 01:21:47 -0600

    Actualizando el archivo que contiene la funcion que abre y lee el archivo de texto de las Alergias
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

--- Tareas/TAREA_CUATRO/src/archivo.py
+++ Tareas/TAREA_CUATRO/src/archivo.py
@@ -1,15 +1,19 @@
+#se importa el modulo para manejar rutas de archivos
+import os
 #se define a la funcion que tiene como parametro el nombre del archivo
 def leer_archivo(nombre_archivo):
     #lista vacia
     alergias_lista = []
+    # obtiene la ruta absoluta del directorio
+    directorio_actual = os.path.dirname(os.path.abspath(__file__))
+    # la ruta completa al archivo
+    ruta_completa = os.path.join(directorio_actual, nombre_archivo)
     #se van a manejar excepciones
     try:
         #se abre el archivo en modo lectura, la codificacion utf-8 es para que acepte tildes
-        with open(nombre_archivo, 'r', encoding='utf-8') as archivo:
-            # lee todas las lineas del archivo y se guardan en la variable
-            lineas = archivo.readlines()
+        with open(ruta_completa, 'r', encoding='utf-8') as archivo:
             #el ciclo for itera entre cada linea de la variable lineas
-            for linea in lineas:
+            for linea in archivo:
                 # verifica que la linea tenga el formato Nombre (valor)
                 if '(' in linea and ')' in linea:
                     # se van a manejar excepciones
@@ -27,9 +31,9 @@ def leer_archivo(nombre_archivo):
                         print(f"\nError al procesar linea {linea.strip()} {str(e)}")
                 #si la linea no tiene el formato se imprime el mensaje
                 else:
-                    print(f"\nError: La línea no sigue el formato esperado: {linea.strip()}")
+                    print(f"\nError: La linea no sigue el formato esperado: {linea.strip()}")
     #si hay un error al intentar abrir el archivo se imprime el mensaje
     except FileNotFoundError:
-        print(f"\nEl archivo {nombre_archivo} no fue encontrado.")
+        print(f"\nEl archivo {ruta_completa} no fue encontrado.")
     #devuelve la lista despues de leer el archivo
     return alergias_lista
