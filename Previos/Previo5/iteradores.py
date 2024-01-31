f459051124501955561a0e7da0ca89a2f16bd5d0
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-31 14:32:13 -0600

    Archivos de iteradores que vana iterar entre colecciones de datos
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo5/iteradores.py
@@ -0,0 +1,13 @@
+# iteradores son metodos que iteran colecciones
+
+# se define la lista
+my_list = [4, 7, 0]
+
+# se crea un iterador de la lista
+iterator = iter(my_list)
+
+# muestra el primer elemento del iterador
+print(next(iterator))
+
+# muestra el siguiente elemento del iterador
+print(next(iterator))
