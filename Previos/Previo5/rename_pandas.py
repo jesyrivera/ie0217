809bb3bcdc2ce3c1a0f340bc28bdc10758f2b5d0
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-31 23:47:21 -0600

    Archivo con ejemplo de renombrar dataFrame en pandas
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo5/rename_pandas.py
@@ -0,0 +1,3 @@
+# se le cambia el nombre a un dataFrame
+# la funcion se aplica a cada nombre de columna y todos los strings son minuscula
+air_quality_renamed = air_quality_renamed.rename(colums=str.lower)
