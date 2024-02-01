6febecefe78eb17489d42edbb83dfb84a5100241
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-01-31 23:19:53 -0600

    Archivo contiene ejemplo de la funcion read_excel para leer datos almacenados como un archivo excel
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

--- Previos/Previo5/leer_excel_pandas.py
+++ Previos/Previo5/leer_excel_pandas.py
@@ -2,3 +2,5 @@ import pandas as pd
 
 # funcion para leer datos almacenados como un archivo excel
 titanic = pd.read_excel("titanic.xlsx", sheet_name="passengers")
+# obtiene la informacion del dataFrame
+titanic.info()
