37dc60979f6c260ed885ffec8debb5f692ab7c1e
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-02-07 04:16:34 -0600

    Actualizando el archivo que contiene las funciones de la segunda parte de la practica que usa funciones NumPy
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

--- Tareas/TAREA_CINCO/src/estudiantes.py
+++ Tareas/TAREA_CINCO/src/estudiantes.py
@@ -9,9 +9,9 @@ def generar_datos():
     # ciclo for que itera 5 veces en cada estudiante
     for i in range(1, 6):
         # genera un arreglo numpy con 5 materias
-        notas = np.random.randint(60, 100, size=5)  # Generar 5 notas aleatorias por estudiante
+        notas = np.random.randint(60, 100, size=5)
         # se agrega los datos a una tupla
-        datos.append(("Estudiante " + str(i), notas))  # Agregar el nombre del estudiante y sus notas a la lista
+        datos.append(("Estudiante " + str(i), notas))
     # devuelva la lista
     return datos
 
@@ -28,7 +28,7 @@ def calcular_promedio(datos):
 
 # se define la funcion
 def obtener_nota_alta(datos):
-    #se usa la funcion np.max para obtener la nota mas alta por estudiante
+    # se usa la funcion np.max para obtener la nota mas alta por estudiante
     nota_alta_por_estudiante = np.max(datos, axis=1)
     # devuelve un arreglo con los resultados
     return nota_alta_por_estudiante
@@ -37,6 +37,6 @@ def obtener_nota_alta(datos):
 # se define la funcion
 def calcular_suma_total(datos):
     # usa la funcion np.sum para sumar los valores de cada materia
-    suma_total_materia= np.sum(datos, axis=0)
+    suma_total_materia = np.sum(datos, axis=0)
     # devuelve un arreglo con los resultados
     return suma_total_materia
