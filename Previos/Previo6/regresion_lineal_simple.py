e3726cd8ab19d95c4edcccb7b7f6f0ea448a78d1
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-02-07 17:49:08 -0600

    Ejemplo de regresion lineal simple donde se establece una relacion entre una variable independiente(entrada) y una dependiente(salida)
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

---
+++ Previos/Previo6/regresion_lineal_simple.py
@@ -0,0 +1,33 @@
+# importar bibliotecas
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# se generan numeros al azar pero siempre los mismos
+np.random.seed(42)
+# se genera un vector de 100 numeros al azar multiplicado
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# se crea un modelo con la instancia un objeto LinearRegression
+modelo = LinearRegression()
+
+# ajusta el modelo, le paso los parametros X y 'y'
+modelo.fit(X, y)
+
+# imprime la informacion y los atributos dentro de modelo.fit y se acceden de esa manera
+print("Coeficiente:", modelo.coef_[0][0])
+print("Intercepcion:", modelo.intercept_[0])
+
+
+# grafico de dispersion
+plt.scatter(X, y)
+# grafico de linea de color 3
+plt.plot(X, modelo.predict(X), color='red', linewidth=3)
+# titulo
+plt.title('Regresion Lineal')
+# etiquetas
+plt.xlabel('X')
+plt.ylabel('y')
+# lo muestra
+plt.show()
