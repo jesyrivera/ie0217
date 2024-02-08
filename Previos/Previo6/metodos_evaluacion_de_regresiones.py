a3c76fc6752d8a3b5be3943bbfd6a35576cc9e76
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-02-07 22:43:27 -0600

    Ejemplo donde se calcula el error: medio absoluto, cuadratico medio, absoluto relativo, cuadratico relativo, raiz cuadratica dle error cuadratico medio
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

--- Previos/Previo6/metodos_evaluacion_de_regresiones.py
+++ Previos/Previo6/metodos_evaluacion_de_regresiones.py
@@ -1,6 +1,6 @@
 # se importan las bibliotecas
 import numpy as np
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -12,8 +12,8 @@ np.random.seed(42)
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
 
-# los datos de ejemplo que se crearon, se dividen en conjuntos de entrenamiento y prueba
-# se generan 4 vectores
+# los datos de ejemplo que se crearon, se dividen en conjuntos de
+# entrenamiento y prueba, se generan 4 vectores
 X_train, X_test, y_train, y_test = \
     train_test_split(X, y, test_size=0.2, random_state=42)
 
