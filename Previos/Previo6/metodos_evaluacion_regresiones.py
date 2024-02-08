5a8b2b47545581a77ee98c91af76646749231163
Jesy Rivera <jesy.rivera@ucr.ac.cr>
2024-02-07 22:47:05 -0600

    Ejemplo de metodos de evaluacion de regresiones del coeficiente de determinacion R^2
    Signed-off-by: Jesy Rivera <jesy.rivera@ucr.ac.cr>

--- Previos/Previo6/metodos_evaluacion_regresiones.py
+++ Previos/Previo6/metodos_evaluacion_regresiones.py
@@ -1,6 +1,6 @@
 # se importan las bibliotecas
 import numpy as np
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
@@ -11,8 +11,8 @@ np.random.seed(42)
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
 
-# los datos de ejemplo que se crearon, se dividen en conjuntos de entrenamiento y prueba
-# se generan 4 vectores
+# los datos de ejemplo que se crearon, se dividen en conjuntos de
+# entrenamiento y prueba, se generan 4 vectores
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
@@ -31,8 +31,7 @@ r2 = r2_score(y_test, y_pred)
 # gradico de dispersion
 plt.scatter(X_test, y_test, label='Datos de prueba', color='blue')
 # grafico de linea
-plt.plot(X_test, y_pred, label=f'Regresion lineal (R^2={r2:.2f})', 
-        color='red')
+plt.plot(X_test, y_pred, label=f'Regresion lineal (R^2={r2:.2f})', color='red')
 # titulo
 plt.title('Regresion Lineal y Coeficientes de Determinacion R^2')
 # etiquetas
